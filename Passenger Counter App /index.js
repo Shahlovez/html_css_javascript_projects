a4a7a32504d2789a1d3cbe7c272ea19c270230c7
Shahlo Ovezniyazova <ovezniyazova@gmail.com>
2022-08-05 23:52:55 -0400

    Updated design and functionality

---
+++ Passenger Counter App /index.js
@@ -0,0 +1,14 @@
+let countEl = document.getElementById("count-el")
+
+console.log(countEl)
+
+let count = 0
+
+function increment(){
+    count = count + 1
+    countEl.innerText = count
+}
+
+function save(){
+    console.log(count)
+}
